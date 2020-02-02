7f6e167c2b2b66f5072b50ac3a34dfe806a0663a
larikkai <kai.larikka@kolumbus.fi>
2020-02-02 23:28:43 +0200

    Add App module

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,58 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Course from './components/Course'
+import App from './App'
 
-const App = () => {
-    const courses = [
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
         {
-          name: 'Half Stack application development',
-          id: 1,
-          parts: [
-            {
-              name: 'Fundamentals of React',
-              exercises: 10,
-              id: 1
-            },
-            {
-              name: 'Using props to pass data',
-              exercises: 7,
-              id: 2
-            },
-            {
-              name: 'State of a component',
-              exercises: 14,
-              id: 3
-            },
-            {
-              name: 'Redux',
-              exercises: 11,
-              id: 4
-            }
-          ]
-        }, 
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
         {
-          name: 'Node.js',
-          id: 2,
-          parts: [
-            {
-              name: 'Routing',
-              exercises: 3,
-              id: 1
-            },
-            {
-              name: 'Middlewares',
-              exercises: 7,
-              id: 2
-            }
-          ]
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
         }
       ]
-  
-    return (
-      <div>
-        <Course courses={courses} />
-      </div>
-    )
-  }
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App courses={courses}/>, document.getElementById('root'))
