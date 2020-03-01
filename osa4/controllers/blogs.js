451299f26c663cf2019fa3d3b60edb898a18b34f
larikkai <kai.larikka@kolumbus.fi>
2020-03-01 12:36:44 +0200

    Clear working branch (random fixes)

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -26,7 +26,7 @@ blogsRouter.post('/', async (request, response) => {
   }
 
   const user = await User.findById(decodedToken.id)
-  
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
